626743885642f6f5fc87e658e0faf37d7ec4c863
Claudio <auron.tidus@gmail.com>
2019-08-10 17:51:08 +0200

    add comment

--- app/src/main/java/com/example/android/earthquakeapp/MainActivity.java
+++ app/src/main/java/com/example/android/earthquakeapp/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.android.earthquakeapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+TODO Programmable Web API Directory: http://www.programmableweb.com/apis/directory
+TODO Google APIs Explorer: https://developers.google.com/apis-explorer/#p/
+TODO Data.gov: http://data.gov
+TODO Tips for building a great UI https://developer.android.com/guide/topics/ui
+TODO USGS Earthquake real time feeds and notifications: http://earthquake.usgs.gov/earthquakes/feed/v1.0/index.php
+TODO USGS Real-Time Earthquake Data in Spreadsheet Format: http://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
+*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
